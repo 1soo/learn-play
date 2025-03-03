6125bb7de9a3e2f537d114a6d27b4854d210dcf5
1soo <aaa4799@naver.com>
2025-03-03 14:51:10 +0900

    chore: Initialize

---
+++ src/main/java/com/sgp/norara/NoraraApplication.java
@@ -0,0 +1,13 @@
+package com.sgp.norara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoraraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoraraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sgp/norara/NoraraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgp.norara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoraraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
